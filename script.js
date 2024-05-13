7b272779912ac494875182944561743ae34d947e
wingklevega16@gmail.com <wingklevega16@gmail.com>
2024-05-13 22:22:46 +0800

    h4 changing

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
     data.education.forEach(edu => {
         const eduDiv = document.createElement('div');
         eduDiv.innerHTML = `
-            <p>${edu.school}</p>
+            <h4>${edu.school}</4>
             <p>${edu.year}</p>
         `;
         educationContainer.appendChild(eduDiv);
