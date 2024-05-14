c63bc33b38669cb0b44f98951a0dc327adb235f2
wingklevega16@gmail.com <wingklevega16@gmail.com>
2024-05-14 11:57:39 +0800

    test

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         languagesList.appendChild(listItem);*/
         /*const item = document.querySelector("#languages .l_p:nth-type(${index + 1})");
         item.textContent = language.language;*/
-        const langItem = languagesList.querySelectorAll('.l_p')[index];
-        langItem.querySelector('.language').textContent = language.language;
+        const langItem = languagesList.querySelectorAll('.l_p').textContent = language.language;
       });
 
       // Update personal info
